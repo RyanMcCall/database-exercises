2a593d35acb153b34928aa9fbed12b374bf8bf02
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-07 14:54:00 -0600

    Complete Excercise 3.3

---
+++ 3.3_mysql_introduction.sql
@@ -0,0 +1,17 @@
+USE `employees`;
+
+SHOW TABLES;
+
+SELECT * FROM employees;
+
+# 5. INT, DATE, CHAR, ENUM, DATE
+
+# 6. All
+
+# 7. All
+
+# 8. dept_emp_latest_date, employees, titles, salaries, dept_manager
+
+# 9. dept_emp shows the relationship between employees and departments
+
+SHOW CREATE TABLE dept_manager;
