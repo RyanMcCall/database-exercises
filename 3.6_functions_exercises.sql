5deed8e526f7a52c46b46e55547a854df4f494a7
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-11 10:36:03 -0600

    Format last query

--- 3.6_functions_exercises.sql
+++ 3.6_functions_exercises.sql
@@ -73,7 +73,15 @@ FROM salaries
 -- employees. A username should be all lowercase, and consist of the first character of
 -- the employees first name, the first 4 characters of the employees last name, an underscore,
 -- the month the employee was born, and the last two digits of the year that they were born.
-SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) AS username,
+SELECT LOWER(
+			CONCAT(
+				SUBSTR(first_name, 1, 1), 
+				SUBSTR(last_name, 1, 4),
+				'_', 
+				SUBSTR(birth_date, 6, 2), 
+				SUBSTR(birth_date, 3, 2)
+				)
+			) AS username,
 	first_name,
 	last_name,
 	birth_date
