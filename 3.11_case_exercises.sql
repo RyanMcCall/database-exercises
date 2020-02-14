09bf2d4fcb6f6b46f0247c751693ac990e211f93
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-14 15:02:27 -0600

    Complete 3.11

--- 3.11_case_exercises.sql
+++ 3.11_case_exercises.sql
@@ -18,3 +18,25 @@ SELECT CONCAT(first_name, " ", last_name) as employee_full_name,
     END AS alpha_group
 FROM employees;
 
+-- How many employees were born in each decade?
+SELECT CASE 
+	WHEN birth_date LIKE "195%" THEN "1950s"
+	WHEN birth_date LIKE "196%" THEN "1960s"
+	ELSE "other"
+	END AS birth_decade,
+COUNT(*)
+FROM employees
+GROUP BY birth_decade;
+
+-- What is the average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
+SELECT AVG(s.salary),
+        CASE 
+            WHEN dept_name IN ('research', 'development') THEN 'R&D'
+            WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing' 
+            WHEN dept_name IN ('Production', 'Quality Management') THEN 'Prod & QM'
+            ELSE dept_name
+            END AS dept_group
+FROM dept_emp AS de 
+JOIN departments AS d ON de.dept_no = d.dept_no
+JOIN salaries AS s ON s.emp_no = de.emp_no
+GROUP BY dept_group;
