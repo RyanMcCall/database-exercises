20fcfbc2029e6b930e5c0c59194969580797df69
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-16 14:05:00 -0600

    Add more solutions to sakila db exercises

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -108,5 +108,23 @@ SELECT LOWER(first_name), LOWER(last_name)
 FROM actor;
 
 -- You need to find the ID number, first name, and last name of an actor, of whom you know
---  only the first name, "Joe." What is one query would you could use to obtain this 
+--  only the first name, "Joe." What is one query you could use to obtain this 
 -- information?
+SELECT actor_id, first_name, last_name
+FROM actor
+WHERE first_name = "Joe";
+
+-- Find all actors whose last name contain the letters "gen":
+SELECT *
+FROM actor
+WHERE last_name LIKE "%gen%";
+
+-- Find all actors whose last names contain the letters "li". This time, order the rows
+-- by last name and first name, in that order.
+SELECT *
+FROM actor
+WHERE last_name LIKE "%li%"
+ORDER BY last_name, first_name;
+
+-- Using IN, display the country_id and country columns for the following countries:
+--  Afghanistan, Bangladesh, and China.
