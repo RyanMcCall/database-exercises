77181921a4e9c93c3a2295ebc6621efd5a9ee920
Ryan McCall <ryan.e.mccall@gmail.com>
2020-02-16 10:25:46 -0600

    Answer some sakila db questions

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -103,3 +103,10 @@ JOIN country co ON co.Code = ci.CountryCode;
 -- Sakila Database
 USE sakila;
 
+-- Display the first and last names in all lowercase of all the actors.
+SELECT LOWER(first_name), LOWER(last_name)
+FROM actor;
+
+-- You need to find the ID number, first name, and last name of an actor, of whom you know
+--  only the first name, "Joe." What is one query would you could use to obtain this 
+-- information?
